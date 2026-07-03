a596927232cb5182f3397e5da4e4c584a8e7848c
sofytorresuu-hash <sofytorresu.u@gmail.com>
2026-07-03 01:36:10 +0000

    wip:s1 script

---
+++ s1.sh
@@ -0,0 +1,4 @@
+#el comando uname-a despliega la version del SO,la fecha en la que se creó, la version,etc.
+uname -a
+#el comando echo shell se usa para ver la ruta del intérprete de comandos.
+echo $SHELL 
